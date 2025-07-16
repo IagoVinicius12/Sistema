1db1e03a65407126bf15531a51518fdc6bbdb605
IagoVinicius12 <erolnazori@gmail.com>
2025-07-16 19:05:25 -0300

    inicio:Routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css'
-import Login from './Pages/Login'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './Pages/Login';
+import Create_ACC from './Pages/Create_ACC';
 
 
 function App() {
   return (
-    <>
-      <Login></Login>
-    </>
-  )
+    <Router>
+      <nav>
+        <Link to="/">Login</Link>
+        <Link to="/about">Create</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/about" element={<Create_ACC />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App
