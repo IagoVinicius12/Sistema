ce064008ea21baf247ca266a67b8653b6ef92d78
IagoVinicius12 <erolnazori@gmail.com>
2025-07-19 13:22:43 -0300

    testando o carregamento

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,10 +1,15 @@
 import Input from '../Components/Input'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Submit_get_users, Submit_login, type User } from '../Requisitions/login'
 
 
 export default function Home() {
     const [users, setUsers] = useState<User[]>()
+
+    useEffect(()=>{
+        handle_get_all()
+    },[])
+
     const handle_get_all = async () => {
         try {
             const token: string | null = localStorage.getItem('authToken')
@@ -16,33 +21,31 @@ export default function Home() {
                 throw new Error("Dados de usuários inválidos recebidos do servidor");
             }
 
-            setUsers(users_vet); // Agora isso vai funcionar corretamente
+            setUsers(users_vet);
         } catch (err) {
             throw new Error('Internal Server Error')
         }
     }
     return (
         <>
-            <div className='flex flex-col justify-center items-center bg-white h-svh' onLoad={handle_get_all}>
+            <div className='flex flex-col justify-center items-center bg-white h-svh'>
                 <div className='bg-white h-80 w-lg flex flex-col justify-center items-center rounded-lg gap-4'>
                     <button className='bg-green-300 w-16 h-8 rounded-lg' onClick={handle_get_all}>get</button>
                 </div>
                 <table className="min-w-full bg-white border border-gray-200">
                     <thead>
                         <tr className="bg-gray-100">
-                            <th className="py-2 px-4 border-b">ID</th>
-                            <th className="py-2 px-4 border-b">Nome</th>
-                            <th className="py-2 px-4 border-b">Email</th>
-                            <th className="py-2 px-4 border-b">Idade</th>
+                            <th className="py-2 px-4 border-b text-center">ID</th>
+                            <th className="py-2 px-4 border-b text-center">Nome</th>
+                            <th className="py-2 px-4 border-b text-center">Email</th>
                         </tr>
                     </thead>
                     <tbody>
                         {users?.map((user,index) =>
                             <tr key={index} className="hover:bg-gray-50">
-                                <td className="py-2 px-4 border-b text-center"></td>
-                                <td className="py-2 px-4 border-b">aa</td>
-                                <td className="py-2 px-4 border-b">aaaaaaaa</td>
-                                <td className="py-2 px-4 border-b text-center">aaaaaaaaaaaaa</td>
+                                <td className="py-2 px-4 border-b text-center">{user.id}</td>
+                                <td className="py-2 px-4 border-b text-center">{user.name}</td>
+                                <td className="py-2 px-4 border-b text-center">{user.email}</td>
                             </tr>
                         )}
                     </tbody>
